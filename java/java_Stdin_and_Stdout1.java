692bfef9d9429ad900442cd35d8df6000870518d
manishshingre <shingre.manish11@gmail.com>
2022-09-15 19:20:14 +0530

    2 questions solved

---
+++ java/java_Stdin_and_Stdout1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class java_Stdin_and_Stdout1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
