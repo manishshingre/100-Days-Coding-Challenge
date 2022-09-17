1018e06d5ecbf32e4e503ed33854c686f4343dda
manishshingre <shingre.manish11@gmail.com>
2022-09-17 20:37:58 +0530

    Update java_Output_Formatting.java

--- java/java_Output_Formatting.java
+++ java/java_Output_Formatting.java
@@ -8,8 +8,21 @@ public class java_Output_Formatting {
             String s1 = sc.next();
             int x = sc.nextInt();
             // Complete this line
+            int size = s1.length();
+            System.out.print(s1);
+            for (int j = size; j < 15; j++) {
+                System.out.print(" ");
+
+            }
+            if (x < 10) {
+                System.out.println("00" + x);
+            } else if (x < 100) {
+                System.out.println("0" + x);
+            } else {
+                System.out.println(x);
+            }
         }
         System.out.println("================================");
-
+        sc.close();
     }
 }
